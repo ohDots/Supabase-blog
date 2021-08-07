0b52c1497f3219abd343f64ae0bcbb55b0a4ee3c
ohDots <nathan@augmintedlabs.io>
2021-08-07 16:33:07 -0500

    Updated env

--- src/client.js
+++ src/client.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
+require('dotenv').config()
+const API_URL = process.env.API_URL
+const API_KEY = process.env.API_KEY
 
 export const supabase = createClient(
-  //Use ENV Vars
+  API_URL,API_KEY
 )
